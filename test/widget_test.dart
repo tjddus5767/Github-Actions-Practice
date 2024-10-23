317945aeeb3636a0a9a8270878ac3f225cb90b7b
tjddus <jb5767sy123@gmail.com>
2024-10-23 16:52:55 +0900

    test파일 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,16 +8,16 @@ void main() {
     // 앱을 빌드하고 화면에 표시
     await tester.pumpWidget(MyApp());
 
-    // 처음 카운터가 0인지 확인
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // 처음 카운터가 "버튼 클릭 횟수: 0"으로 표시되는지 확인
+    expect(find.text('버튼 클릭 횟수: 0'), findsOneWidget);
+    expect(find.text('버튼 클릭 횟수: 1'), findsNothing);
 
     // 버튼을 눌러서 카운터 증가
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // 카운터가 1로 증가했는지 확인
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // 카운터가 "버튼 클릭 횟수: 1"로 증가했는지 확인
+    expect(find.text('버튼 클릭 횟수: 0'), findsNothing);
+    expect(find.text('버튼 클릭 횟수: 1'), findsOneWidget);
   });
 }
